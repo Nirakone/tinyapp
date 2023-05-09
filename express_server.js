7efe0e3f25c86d8a3ba306c717fecb3ce2fa18c4
Nirakone Phromkharanourak <119075500+Nirakone@users.noreply.github.com>
2023-05-08 21:56:26 -0400

    file now makes short urls and redirects to page

--- express_server.js
+++ express_server.js
@@ -5,6 +5,16 @@ const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
 
 
+function generateRandomString() {
+  const possibleChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let randomString = '';
+  for (let i = 0; i < 6; i++) {
+    let randomIndex = Math.floor(Math.random() * possibleChars.length);
+    randomString += possibleChars.charAt(randomIndex);
+  }
+  return randomString;
+}
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -17,6 +27,9 @@ const urlDatabase = {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+app.use(express.urlencoded({ extended: true }));
+
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
@@ -31,7 +44,28 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  //res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  const newId = generateRandomString();
+  urlDatabase[newId] = req.body.longURL;
+  res.redirect(`/urls/${newId}`);
+});
+
+app.get("/u/:id", (req, res) => {
+  const longURL = urlDatabase[req.params.id];
+  if (longURL) {
+    res.redirect(longURL);
+  } else {
+    res.status(404).send("URL not found");
+  }
 });
