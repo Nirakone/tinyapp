19d913e98ebb506484dcae4456290d3cfa67d1db
Nirakone Phromkharanourak <119075500+Nirakone@users.noreply.github.com>
2023-06-02 23:59:07 -0400

    require cookie-parser added routes

--- express_server.js
+++ express_server.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
 const app = express();
 const PORT = 8080; // default port 8080
 
+
 app.set("view engine", "ejs");
+app.use(cookieParser());
 
 
 function generateRandomString() {
@@ -40,13 +43,19 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { urls: urlDatabase, username: req.cookies.username };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  
+  const templateVars = {
+    username: req.cookies["username"],
+    
+  };
+  res.render("urls_new", templateVars);
 });
+
 
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
@@ -85,6 +94,14 @@ app.post("/urls/:id", (req, res) => {
 
 app.post("/login",(req,res) => {
   console.log(req.body);
-  res.cookie("username",req.body);
+  res.cookie("username",req.body.username);
   res.redirect("/urls");
+});
+
+app.get("/urls", (req, res) => {
+  const templateVars = {
+    username: req.cookies["username"],
+    
+  };
+  res.render("urls_index", templateVars);
 });
